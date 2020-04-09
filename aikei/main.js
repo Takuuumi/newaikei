53a3be083da705dafe8e9d7fafd6fd57eae188f6
Takuuumi <58387890+Takuuumi@users.noreply.github.com>
2020-04-09 21:28:37 +0900

    Add files via upload

---
+++ aikei/main.js
@@ -0,0 +1,12 @@
+// document.addEventListener('DOMContentLoaded', function() {
+//     document.getElementById("menuButton").addEventListener("click", function() {
+//         this.classList.toggle("active");
+//         document.getElementById("nav").classList.toggle("active");
+//     })    
+// });
+document.addEventListener('DOMContentLoaded', function() {
+    document.getElementById("menuButton").addEventListener("click", function() {
+        this.classList.toggle("active");
+        document.getElementById("v-nav").classList.toggle("active");
+    })
+});
